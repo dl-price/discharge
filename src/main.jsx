e5a9dca728db1d79351c8459091842f9ee6dd5b0
Dan Price <dan.l.price@me.com>
2025-12-20 09:49:39 +1000

    Fixed github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App.jsx";
 import "./index.css";
@@ -67,9 +67,9 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
